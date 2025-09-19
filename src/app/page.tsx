97a335682b02c92bdd3268f8569e177980714c68
gt3oliveira <gt3oliveira@gmail.com>
2025-09-19 17:35:49 -0300

    chore: add shadcn setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold text-red-500 underline">
-        Hello, Next.js!
-      </h1>
+    <div className="flex flex-col items-center justify-center gap-8 py-10">
+      <h1 className="text-3xl font-bold text-red-500">Hello, Next.js!</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
