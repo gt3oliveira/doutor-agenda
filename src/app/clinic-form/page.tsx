4680c504c9e9912994cb76b88a8c93d8cc25c61f
gt3oliveira <gt3oliveira@gmail.com>
2025-09-22 22:20:24 -0300

    feat: add create clinics form page

---
+++ src/app/clinic-form/page.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+import { ClinicForm } from "./components/form";
+
+export default function ClinicFormPage() {
+  return (
+    <Dialog open>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Adicionar Clínica</DialogTitle>
+          <DialogDescription>
+            Por favor, adicione uma clínica para começar a usar o sistema.
+          </DialogDescription>
+        </DialogHeader>
+        <ClinicForm />
+      </DialogContent>
+    </Dialog>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,11 @@
+import { eq } from "drizzle-orm";
 import { headers } from "next/headers";
+import Image from "next/image";
 import { redirect } from "next/navigation";
 import React from "react";
 
+import { db } from "@/db";
+import { usersToClinicsTable } from "@/db/schema";
 import { auth } from "@/lib/auth";
 
 import { SignOutButton } from "./components/sign-out-button";
@@ -15,10 +19,25 @@ export default async function DashboardPage() {
     redirect("/authentication");
   }
 
+  const clinics = await db.query.usersToClinicsTable.findMany({
+    where: eq(usersToClinicsTable.userId, session.user.id),
+  });
+
+  if (clinics.length === 0) {
+    redirect("/clinic-form");
+  }
+
   return (
     <div>
       <h1>DashboardPage</h1>
       <span>{session.user.name}</span>
+      <Image
+        src={session.user.image as string}
+        alt={session.user.name}
+        width={50}
+        height={50}
+        className="rounded-full"
+      />
       <SignOutButton />
     </div>
   );
