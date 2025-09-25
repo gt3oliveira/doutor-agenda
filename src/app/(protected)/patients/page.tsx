efab0a41da3e2f2fca2e1ed581997504cb36fec9
gt3oliveira <gt3oliveira@gmail.com>
2025-09-25 20:01:46 -0300

    feat: add crud e listagem da pagina de pacientes

--- src/app/(protected)/patients/page.tsx
+++ src/app/(protected)/patients/page.tsx
@@ -1,5 +1,53 @@
-import React from "react";
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function PatientsPage() {
-  return <div>PatientsPage</div>;
+import {
+  PageActions,
+  PageContainer,
+  PageContent,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/components/page-container";
+import { DataTable } from "@/components/ui/data-table";
+import { db } from "@/db";
+import { patientsTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
+
+import AddPatientButton from "./_components/add-patient-button";
+import { patientsTableColumns } from "./_components/table-columns";
+
+export default async function PatientsPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+  if (!session.user.clinic) {
+    redirect("/clinic-form");
+  }
+  const patients = await db.query.patientsTable.findMany({
+    where: eq(patientsTable.clinicId, session.user.clinic.id),
+  });
+  return (
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Pacientes</PageTitle>
+          <PageDescription>
+            Gerencie os pacientes da sua clínica
+          </PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <AddPatientButton />
+        </PageActions>
+      </PageHeader>
+      <PageContent>
+        <DataTable data={patients} columns={patientsTableColumns} />
+      </PageContent>
+    </PageContainer>
+  );
 }
