f6da6233f89a5efa1b2958709bb037b7f7a2a965
gt3oliveira <gt3oliveira@gmail.com>
2025-09-27 16:47:42 -0300

    fix: updated text of not results in table

--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -69,7 +69,7 @@ export function DataTable<TData, TValue>({
           ) : (
             <TableRow>
               <TableCell colSpan={columns.length} className="h-24 text-center">
-                A clínica não possui pacientes.
+                Nenhum resultado encontrado.
               </TableCell>
             </TableRow>
           )}
